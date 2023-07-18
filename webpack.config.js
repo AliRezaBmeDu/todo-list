925558158c32b1141fa66a6686cb8c7ffa1b1f3b
AliReza <ali.reza.buet@gmail.com>
2023-07-18 15:52:21 +0600

    Create the local development server

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  entry: {
+    main: './src/index.js'
+  },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
         template: './src/index.html',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
